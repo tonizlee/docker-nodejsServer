c2464dec42a157a75d4e2fd3edf7c691dba94bb1
tonizlee <ezCareToni@gmail.com>
2018-06-19 14:15:06 +0900

    Docker Test Node Server

---
+++ DockerWS-nodejs/index.js
@@ -0,0 +1,54 @@
+var http = require('http');
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host:"localhost",
+    port:3306,
+    user : "root",
+    password : "1234",
+    database : "TestingDB"
+})
+
+connection.query('select * from TestTB', function (err, rows, fields)
+{
+    console.log(rows);
+});
+
+// 웹 서버 객체를 만듭니다.
+var server = http.createServer();
+
+//웹 서버를 시작하여 3000번 포트에서 대기하도록 설정합니다.
+var port = 3000;
+server.listen(port, function(){
+    console.log('웹 서버가 시작되었습니다. : %d', port);
+});
+
+//클라이언트 연결 이벤트 처리
+server.on('connection', function(socket){
+    var addr = socket.address();
+    console.log('클라이언트가 접속 : %s, %d', addr.address, addr.port);
+});
+
+//클라이언트 요청 이벤트 처리
+server.on('request', function(req, res){
+    console.log('클라이언트 요청이 들어왔습니다.');
+    
+    res.writeHead(200,{"Content-Type": "text/html; charset=utf-8"});
+    res.write("<!DOCTYPE html>");
+    res.write("<html>");
+    res.write("  <head>");
+    res.write("    <title>Simple Web Server with node js</title>");
+    res.write("  </head>");
+    res.write("  <body>");
+    res.write("   <h1>Node JS 부터의 응답 페이지!!!</h1>");
+    res.write("  </body>");
+    res.write("</html>");
+    res.end();
+
+
+});
+
+//서버 종료 이벤트 처리
+server.on('close', function(){
+    console.log('서버가 종료됩니다.');
+});
